855f92fc329776385240648fb6634ba06f431529
WillLantz <willlantz@mail.weber.edu>
2020-11-28 14:36:57 -0700

    Adding all files

---
+++ DieGuessGame/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace DieGuessGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rollButton = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.GameInfo = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userGuessBox = new System.Windows.Forms.TextBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.errorBox = new System.Windows.Forms.Label();
+            this.dice = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dice)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rollButton
+            // 
+            this.rollButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.rollButton.FlatAppearance.BorderSize = 10;
+            this.rollButton.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.rollButton.Location = new System.Drawing.Point(217, 297);
+            this.rollButton.Name = "rollButton";
+            this.rollButton.Size = new System.Drawing.Size(109, 40);
+            this.rollButton.TabIndex = 0;
+            this.rollButton.Text = "Roll";
+            this.rollButton.UseVisualStyleBackColor = true;
+            this.rollButton.Click += new System.EventHandler(this.rollButton_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.Location = new System.Drawing.Point(46, 448);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(428, 125);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // GameInfo
+            // 
+            this.GameInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameInfo.Location = new System.Drawing.Point(27, 25);
+            this.GameInfo.Name = "GameInfo";
+            this.GameInfo.Padding = new System.Windows.Forms.Padding(0);
+            this.GameInfo.Size = new System.Drawing.Size(200, 120);
+            this.GameInfo.TabIndex = 2;
+            this.GameInfo.TabStop = false;
+            this.GameInfo.Text = "Game Info";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(24, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter your guess (1-6):\r\n";
+            // 
+            // userGuessBox
+            // 
+            this.userGuessBox.Location = new System.Drawing.Point(237, 234);
+            this.userGuessBox.MaxLength = 1;
+            this.userGuessBox.Name = "userGuessBox";
+            this.userGuessBox.Size = new System.Drawing.Size(68, 22);
+            this.userGuessBox.TabIndex = 4;
+            // 
+            // resetButton
+            // 
+            this.resetButton.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.resetButton.Location = new System.Drawing.Point(217, 362);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(109, 44);
+            this.resetButton.TabIndex = 5;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // errorBox
+            // 
+            this.errorBox.AutoSize = true;
+            this.errorBox.Location = new System.Drawing.Point(326, 237);
+            this.errorBox.Name = "errorBox";
+            this.errorBox.Size = new System.Drawing.Size(0, 17);
+            this.errorBox.TabIndex = 6;
+            // 
+            // dice
+            // 
+            this.dice.Location = new System.Drawing.Point(46, 297);
+            this.dice.Name = "dice";
+            this.dice.Size = new System.Drawing.Size(144, 109);
+            this.dice.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.dice.TabIndex = 7;
+            this.dice.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(531, 600);
+            this.Controls.Add(this.dice);
+            this.Controls.Add(this.errorBox);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.userGuessBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GameInfo);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.rollButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Die Guessing Game";
+            ((System.ComponentModel.ISupportInitialize)(this.dice)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button rollButton;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.GroupBox GameInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userGuessBox;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label errorBox;
+        private System.Windows.Forms.PictureBox dice;
+    }
+}
+
---
+++ DieGuessGame/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DieGuessGame
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //All Text Box headers
+            richTextBox2.Text = ("Face\t     Frequency     Percent     Number Of Times Guessed");
+            GameInfo.Text = $"Game Info\n\nAmount of times played: {rollCount} \n\nAmount of time won: {wincount}\n\nAmount of times lost: {lossCount}";
+
+        }
+
+
+
+        /// <summary>
+        /// All variable initialization is stored in this region
+        /// </summary>
+        #region VariableInitialization
+
+        private int rollCount = 0; // initializing roll count outside of the rollButton_click method
+        private int wincount = 0; // initializing win count
+        private int lossCount = 0; // intitializing loss count
+
+
+        private int frequency1 = 0; // count of 1's rolled
+        private int frequency2 = 0; // count of 2's rolled
+        private int frequency3 = 0; // count of 3's rolled
+        private int frequency4 = 0; // count of 4's rolled
+        private int frequency5 = 0; // count of 5's rolled
+        private int frequency6 = 0; // count of 6's rolled
+
+        private int guessNumber1 = 0; // number of times 1 is guessed
+        private int guessNumber2 = 0; // number of times 2 is guessed
+        private int guessNumber3 = 0; // number of times 3 is guessed
+        private int guessNumber4 = 0; // number of times 4 is guessed
+        private int guessNumber5 = 0; // number of times 5 is guessed
+        private int guessNumber6 = 0; // number of times 6 is guessed
+
+        private double percent1 = 0; // percent of 1 rolled
+        private double percent2 = 0; // percent of 2 rolled
+        private double percent3 = 0; // percent of 3 rolled
+        private double percent4 = 0; // percent of 4 rolled
+        private double percent5 = 0; // percent of 5 rolled
+        private double percent6 = 0; // percent of 6 rolled
+
+        #endregion VariableInitialization
+
+        /// <summary>
+        /// This method displays all of the data from the game
+        /// </summary>
+        private void displayResults()
+        {
+            richTextBox2.Text = ("Face\t     Frequency     Percent     Number Of Times Guessed");
+            richTextBox2.Text += Environment.NewLine + ($"1\t\t{frequency1}\t     {percent1.ToString("P")}\t\t{guessNumber1}\n2\t\t{frequency2}\t     {percent2.ToString("P")}\t\t{guessNumber2}");
+            richTextBox2.Text += Environment.NewLine + ($"3\t\t{frequency3}\t     {percent3.ToString("P")}\t\t{guessNumber3}\n4\t\t{frequency4}\t     {percent4.ToString("P")}\t\t{guessNumber4}");
+            richTextBox2.Text += Environment.NewLine + ($"5\t\t{frequency5}\t     {percent5.ToString("P")}\t\t{guessNumber5}\n6\t\t{frequency6}\t     {percent6.ToString("P")}\t\t{guessNumber6}");
+        }
+
+        /// <summary>
+        /// This method will print everything in the GameInfo box
+        /// </summary>
+        private void displayGameInfoResults()
+        {
+            GameInfo.Text = $"Game Info\n\nAmount of times played: {rollCount} \n\nAmount of time won: {wincount}\n\nAmount of times lost: {lossCount}";
+        }
+
+        /// <summary>
+        /// This reset method sets the game back into it's initial state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+
+            dice.Image = null; // setting the dice image to null when reset is clicked
+
+            userGuessBox.Text = ""; // wiping the game information slate when clicked
+
+            rollCount = 0; // initializing roll count outside of the rollButton_click method
+            wincount  = 0; // initializing win count
+            lossCount = 0; // intitializing loss count
+
+            frequency1 = 0; // count of 1's rolled
+            frequency2 = 0; // count of 2's rolled
+            frequency3 = 0; // count of 3's rolled
+            frequency4 = 0; // count of 4's rolled
+            frequency5 = 0; // count of 5's rolled
+            frequency6 = 0; // count of 6's rolled
+
+            guessNumber1 = 0; // number of times 1 is guessed
+            guessNumber2 = 0; // number of times 2 is guessed
+            guessNumber3 = 0; // number of times 3 is guessed
+            guessNumber4 = 0; // number of times 4 is guessed
+            guessNumber5 = 0; // number of times 5 is guessed
+            guessNumber6 = 0; // number of times 6 is guessed
+
+            percent1 = 0; // percent of 1 rolled
+            percent2 = 0; // percent of 2 rolled
+            percent3 = 0; // percent of 3 rolled
+            percent4 = 0; // percent of 4 rolled
+            percent5 = 0; // percent of 5 rolled
+            percent6 = 0; // percent of 6 rolled
+
+
+            displayResults();
+            displayGameInfoResults();
+            
+        }
+
+        /// <summary>
+        /// This method will trigger the Random Number Generator as well as comparisons to the users choice
+        /// Face is the result of the random roll
+        /// This will also handle trigger the of the gameResults method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void rollButton_Click(object sender, EventArgs e)
+        {
+                 
+            int userGuess;
+
+            try
+            {
+                userGuess = (Int32.Parse(userGuessBox.Text));
+            }
+            catch (Exception)
+            {
+                errorBox.Text = "Must enter a number (1 - 6)";
+                return;
+            }
+
+            if(userGuess <= 0 || userGuess > 6)
+            {
+                errorBox.Text = "Must enter a number (1 - 6)";
+                return;
+            }
+
+            errorBox.Text = "";
+
+            rollCount++; // iterate the rollCount / amount of times played
+
+            int face = randomNumberGenerator();
+
+            gameResults(face, userGuess);
+
+            diceImg(face);
+
+        }
+
+        /// <summary>
+        /// This is the random number generator that will provide a random number. Being called from the rollButton_Click method
+        /// </summary>
+        /// <returns></returns>
+        public int randomNumberGenerator()
+        {   
+            Random randomNumber = new Random();// Setting up the RNG
+            int number = randomNumber.Next(1, 7);
+            return number;
+        }      
+
+        /// <summary>
+        /// This method will update both the game results box at the bottom and the gameinfo box at the top
+        /// This method is being passed in the 'face' value from the randomNumberGenerator
+        /// It is also being passed in the userGuess
+        /// </summary>
+        /// <param name="x"></param>
+        public void gameResults(int faceValue, int userGuess)
+        {
+            #region GameInfo
+
+            #region GameInfoBox
+           
+            if(userGuess == faceValue)
+            {
+                ++wincount;
+            }
+            else
+            {
+                ++lossCount;
+            }
+
+            displayGameInfoResults();
+            #endregion GameInfoBox
+
+            #region FrequencyStats
+
+
+            //keep track of what number was rolled and increment appropriate counter
+            switch (faceValue)
+            {
+                case 1:
+                    ++frequency1; // increment 1
+                    break;
+                case 2:
+                    ++frequency2; // increment 2
+                    break;
+                case 3:
+                    ++frequency3; // increment 3
+                    break;
+                case 4:
+                    ++frequency4; // increment 4
+                    break;
+                case 5:
+                    ++frequency5; // increment 5
+                    break;
+                case 6:
+                    ++frequency6; // increment 6
+                    break;
+
+            }
+
+
+            // Ugly print format for the frequency stats. **These need to update / replace numbers, not keep re-printing** 
+            // Also need to send all of these to a function to clean up code
+
+
+            #endregion FrequencyStats
+
+            #region Percent
+
+            percent1 = ((double)frequency1 / rollCount);
+            percent2 = ((double)frequency2 / rollCount);
+            percent3 = ((double)frequency3 / rollCount);
+            percent4 = ((double)frequency4 / rollCount);
+            percent5 = ((double)frequency5 / rollCount);
+            percent6 = ((double)frequency6 / rollCount);
+
+            #endregion Percent
+
+            #region NumberOfTimesGuessed
+
+
+
+            switch (userGuess)
+            {
+                case 1:
+                    ++guessNumber1;
+                    break;
+                case 2:
+                    ++guessNumber2;
+                    break;
+                case 3:
+                    ++guessNumber3;
+                    break;
+                case 4:
+                    ++guessNumber4;
+                    break;
+                case 5:
+                    ++guessNumber5;
+                    break;
+                case 6:
+                    ++guessNumber6;
+                    break;
+            }
+
+            #endregion NumberOfTimesGuessed
+
+            #region PrintingResults
+            displayResults();
+            #endregion PrintingResults
+
+            #endregion GameInfo
+        }
+
+        /// <summary>
+        /// This method randomizes the dice gif presented on the app
+        /// </summary>
+        private void diceImg(int face)
+        {
+
+            dice.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            Random diceNumber = new Random();// Setting up the RNG
+            int j = diceNumber.Next(1, 7);
+
+            for (int i = 1; i < 4; i++)
+            {
+                j = diceNumber.Next(1, 7);
+                dice.Image = Image.FromFile("die" + j.ToString() + ".gif");
+                dice.Refresh();
+                Thread.Sleep(200);
+            }
+
+            dice.Image = Image.FromFile("die" + face.ToString() + ".gif");
+
+
+
+        }
+
+    }
+   
+}
